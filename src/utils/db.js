675a2c577d3d50002de74598ee5849b3fe3b3225
AjengAlyaK <ajenga990@gmail.com>
2023-10-24 10:49:21 +0700

    railway

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerce', {
+// mongoose.connect('mongodb://127.0.0.1:27017/ecommerce', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+mongoose.connect('mongodb://mongo:H5d23ha+F-DggGaB-feCHAgAH-gBFfFf@viaduct.proxy.rlwy.net:52719', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
