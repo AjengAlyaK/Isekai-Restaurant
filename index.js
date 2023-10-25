a2d555d39e80de6a89af9657f339c196209485f6
AjengAlyaK <ajenga990@gmail.com>
2023-10-25 14:34:21 +0700

    ejs

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const User = require('./src/model/user');
 require('./src/utils/db');
 const cors = require('cors');
 const app = express();
 const port = 7000;
 
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
 app.get('/', (req, res)=> {
-    res.send('hello');
-})
+    res.render('index', {
+        title: 'Kafe Saya',
+        layout: 'layouts/main-layout',
+    });
+});
+
+app.get('/product', (req, res)=> {
+    res.render('product', {
+        title: 'Kafe Saya | Produk',
+        layout: 'layouts/main-layout',
+    });
+});
+
+app.get('/promo', (req, res)=> {
+    res.render('promo', {
+        title: 'Kafe Saya | Promo',
+        layout: 'layouts/main-layout',
+    });
+});
+
+app.get('/cart', (req, res)=> {
+    res.render('cart', {
+        title: 'Kafe Saya | Keranjang Belanja',
+        layout: 'layouts/main-layout',
+    });
+});
+
+app.get('/signUp', (req, res)=> {
+    res.render('signUp', {
+        title: 'Kafe Saya | Sign Up',
+        layout: 'layouts/entrance',
+    });
+});
 
 app.get('/user', async (req, res)  =>{
     try {
@@ -21,9 +58,6 @@ app.get('/user', async (req, res)  =>{
     }
     
 });
-
-
-// app.post('/login', login);
 
 app.listen(port, "0.0.0.0", () =>{
     console.log(`listening to port ${port}...`);
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo:14F2DEF-FB5G1faf2dCB1d5GDb4E-3dc@roundhouse.proxy.rlwy.net:38819', {
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
