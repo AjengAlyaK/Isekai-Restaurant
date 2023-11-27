c26b139217c0ffa399ee17b8a98eb53300cb8444
AjengAlyaK <ajenga990@gmail.com>
2023-11-27 23:18:51 +0700

    change picts

--- src/controller/admin.js
+++ src/controller/admin.js
@@ -2,23 +2,24 @@ const CheckoutUser = require('../model/checkout');
 const deleteCheckout = require('../model/checkout');
 const UserReservation = require('../model/reservation');
 const Product = require('../model/product');
+const Users = require('../model/user');
 
 const processOrder = async (req, res) => {
     try {
         const checkoutUser = await CheckoutUser.find();
-        res.render('process-order',{
+        res.render('process-order', {
             title: 'FlavourSpark',
             layout: 'layouts/admin-layout',
             checkoutUser
         });
     } catch (error) {
         console.error('Error get data: ', error);
-        res.status(500).json({ error: 'Failed get data'});
+        res.status(500).json({ error: 'Failed get data' });
     }
 }
 
 const deleteProcessOrder = (req, res) => {
-        deleteCheckout.deleteOne({ _id: req.body.id }).then((result) => {
+    deleteCheckout.deleteOne({ _id: req.body.id }).then((result) => {
         // req.flash('msg', 'Data contact berhasil dihapus!');
         res.redirect('/process-order');
     })
@@ -35,21 +36,21 @@ const processReservation = async (req, res) => {
 
 const deleteProcessR = (req, res) => {
     UserReservation.deleteOne({ _id: req.body.id }).then((result) => {
-    // req.flash('msg', 'Data contact berhasil dihapus!');
-    res.redirect('/process-reservation');
-});
+        // req.flash('msg', 'Data contact berhasil dihapus!');
+        res.redirect('/process-reservation');
+    });
 }
 
-const updateStatusR =async(req, res) => {
+const updateStatusR = async (req, res) => {
     try {
         await Reservation.updateOne(
-            { _id: req.body._id},
+            { _id: req.body._id },
             {
                 $set: {
                     status: req.body.status
                 }
             }
-        ).then((result) =>{
+        ).then((result) => {
             // kirimkan flash message
             // req.flash('msg', 'Data contact berhasil diubah!');
             res.redirect('/process-reservation');
@@ -62,13 +63,13 @@ const updateStatusR =async(req, res) => {
 const updateStatusO = async (req, res) => {
     try {
         await Checkout.updateOne(
-            { _id: req.body._id},
+            { _id: req.body._id },
             {
                 $set: {
                     status: req.body.status
                 }
             }
-        ).then((result) =>{
+        ).then((result) => {
             // kirimkan flash message
             // req.flash('msg', 'Data contact berhasil diubah!');
             res.redirect('/process-order');
@@ -79,20 +80,27 @@ const updateStatusO = async (req, res) => {
 }
 
 const admin = async (req, res) => {
+    const countUsers = await Users.countDocuments({});
+    const countReservation = await UserReservation.countDocuments({});
+    const countFO = await CheckoutUser.countDocuments({});
+
     res.render('admin-index', {
         title: 'FlavourSpark',
         layout: 'layouts/admin-layout',
+        countUsers,
+        countReservation,
+        countFO
     });
-} 
+}
 
 const deleteProduct = (req, res) => {
     Product.deleteOne({ _id: req.body.id }).then((result) => {
-    // req.flash('msg', 'Data contact berhasil dihapus!');
-    res.redirect('/admin-products');
-});
+        // req.flash('msg', 'Data contact berhasil dihapus!');
+        res.redirect('/admin-products');
+    });
 }
 
-const adminProducts =  async (req, res) => {
+const adminProducts = async (req, res) => {
     const products = await Product.find();
     res.render('update-products', {
         title: 'FlavourSpark',
@@ -101,7 +109,7 @@ const adminProducts =  async (req, res) => {
     });
 }
 
-const updateProductI = async(req, res, next) => {
+const updateProductI = async (req, res, next) => {
     try {
         const filenames = Object.values(req.files).map(fileArray => fileArray[0].filename);
         let updateProduct = {
@@ -110,8 +118,8 @@ const updateProductI = async(req, res, next) => {
             category: req.body.category,
             preview: req.body.preview,
             description: req.body.description,
-            pict_product : filenames[0],
-            pict_thumb : filenames[1]
+            pict_product: filenames[0],
+            pict_thumb: filenames[1]
         }
         // console.log(req.file, req.body);
         // if(req.file){
@@ -119,11 +127,11 @@ const updateProductI = async(req, res, next) => {
         //     updateProduct.pict_thumb = filenames[1];
         // }
         await Product.updateOne(
-            { _id: req.body._id},
+            { _id: req.body._id },
             {
                 $set: updateProduct
             }
-        ).then((result) =>{
+        ).then((result) => {
             // kirimkan flash message
             // req.flash('msg', 'Data contact berhasil diubah!');
             res.redirect('/admin-products');
@@ -134,7 +142,7 @@ const updateProductI = async(req, res, next) => {
     }
 }
 
-const addProduct =  async (req, res, next) => {
+const addProduct = async (req, res, next) => {
     try {
         const filenames = Object.values(req.files).map(fileArray => fileArray[0].filename);
 
@@ -152,11 +160,11 @@ const addProduct =  async (req, res, next) => {
         //     updateProduct.pict_thumb = req.file.filename
         // }
         await Product.create(newProduct)
-        .then((result) =>{
-            // kirimkan flash message
-            // req.flash('msg', 'Data contact berhasil diubah!');
-            res.redirect('/admin-products');
-        });
+            .then((result) => {
+                // kirimkan flash message
+                // req.flash('msg', 'Data contact berhasil diubah!');
+                res.redirect('/admin-products');
+            });
     } catch (error) {
         console.error(error);
         res.status(500).send({ error: error.message });
@@ -164,4 +172,4 @@ const addProduct =  async (req, res, next) => {
 }
 
 
-module.exports = { processOrder, deleteProcessOrder, processReservation, deleteProcessR, updateStatusR, updateStatusO, admin, deleteProduct, adminProducts, updateProductI, addProduct  };
+module.exports = { processOrder, deleteProcessOrder, processReservation, deleteProcessR, updateStatusR, updateStatusO, admin, deleteProduct, adminProducts, updateProductI, addProduct };
